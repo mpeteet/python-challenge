d072170383cacb72fcb7fa82e7278f95222cdff9
Mona Peteet <mpeteet@comcast.net>
2020-12-26 21:12:47 -0500

    Script updates.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,9 @@
 import os
 import csv
 
-# Tell the program where the input files is located
+# Tell the program where the input and output files are located
 budgetdata_csv = os.path.join('Resources', 'budget_data.csv')
+PyBank_output = os.path.join('..', 'Analysis', 'PyBank_output.txt')
 
 # These are the lists that will store data for analysis
 months = []
@@ -21,20 +22,39 @@ with open(budgetdata_csv, newline='') as csvfile:
     #Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
+    print(header)
        
     for row in csvreader:
         # What are the total number of months in the dataset?
         month = str(row[0])
         months.append(month)
 
-        #What is net total of Profit/Losses?
+        # What is net total of Profit/Losses?
         profit_loss = int(row[1])
         profits_losses.append(profit_loss)
+
+        # The average change is:
+        # 
+
+        # Find the greatest increase in profits for Feb 2012
+        #profit_losses.max(profit_loss) = increase
+       
+        # Find the greatest decrease in profits for Sep 2013
+        #profit_losses.min(profit_loss) = decrease
+
+
 
 
     print(f'Total Months:', len(months))
     print(f'Total: $', sum(profits_losses)) 
+    print(f'')
+    #print(f'Greatest Increase in Profits:' increase[0], increase[1])
 
+# Open the output file
+#with open(PyBank_output, "w", newline="") as datafile
+
+    #Write to the output file
+    #writer = csv.writer(datafile)
 
         
        
